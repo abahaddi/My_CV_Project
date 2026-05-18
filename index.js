3797143fd6e963cadb235f434b916173972c1eaa
abahaddi <a.bahaddii@gmail.com>
2026-05-18 11:44:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if (closeButton && welcomePopup) {
         closeButton.addEventListener("click", function() {
+            // Effet visuel de disparition
             welcomePopup.style.opacity = "0";
+            
+            // Retrait complet de l'élément après l'effet
             setTimeout(function() {
                 welcomePopup.style.display = "none";
             }, 300);
         });
+    } else {
+        console.error("Éléments de la popup introuvables dans le DOM.");
     }
 
 });
