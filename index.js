934d599916cf5258c6bb86efd88c332f9e399199
abahaddi <a.bahaddii@gmail.com>
2026-05-18 11:26:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 $(document).ready(function() {
-    
-    // 1. Animation d'entrée (Zoom + Opacité) pour le message de bienvenue
-    setTimeout(function() {
-        $('#popupContent').css({
-            'opacity': '1',
-            'transform': 'scale(1)'
-        });
-    }, 200);
 
-    // Fermeture de la popup lors du clic sur le bouton d'action
     $('#closePopup').click(function() {
         $('#welcomePopup').fadeOut(400);
     });
