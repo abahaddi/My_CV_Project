7a90db49fc8004d6ab45a8c3feb176f6bd9632c3
abahaddi <a.bahaddii@gmail.com>
2026-05-18 11:41:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var closeButton = document.getElementById("closePopup");
+    var welcomePopup = document.getElementById("welcomePopup");
 
-    $('#closePopup').click(function() {
-        $('#welcomePopup').fadeOut(400);
-    });
+    if (closeButton && welcomePopup) {
+        closeButton.addEventListener("click", function() {
+            welcomePopup.style.opacity = "0";
+            setTimeout(function() {
+                welcomePopup.style.display = "none";
+            }, 300);
+        });
+    }
 
 });
